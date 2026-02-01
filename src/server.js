07433cdf94155c0b52e000830190a83864379fa7
Mr-Zee <911mrzee@gmail.com>
2026-02-02 02:23:00 +0530

    api issue

--- src/server.js
+++ src/server.js
@@ -26,10 +26,6 @@ app.get("/", (_, res) => res.send("Backend is running ✅"));
 app.use("/api/products", productsRoutes);
 
 const port = process.env.PORT || 5000;
-if (process.env.NODE_ENV !== 'production') {
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-}
-
 app.listen(port, () => console.log(`✅ API running at http://localhost:${port}`));
 
 export default app;
