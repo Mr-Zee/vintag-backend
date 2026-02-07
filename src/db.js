9963205110ce2b1354460eef123d8de3d9e1ef4e
Mr-Zee <911mrzee@gmail.com>
2026-02-08 03:41:16 +0530

    pool issue

--- src/db.js
+++ src/db.js
@@ -1,13 +1,14 @@
 import pkg from 'pg';
 const { Pool } = pkg;
+const { Pool } = require('pg');
 import dotenv from "dotenv";
 dotenv.config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL || process.env.POSTGRES_URL,
   ssl: {
-    rejectUnauthorized: false,
-    sslmode: 'verify-full'
+    sslmode: 'verify-full',
+    rejectUnauthorized: false
   }
 });
 
